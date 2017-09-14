80ee8a4f1d1d662ef88f36f1a60109b98d9ed028
Anders Boghammar <anders@boghammar.com>
2017-09-14 17:40:18 +0200

    bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('views/pages/index');
+  res.render('views/pages/index.ejs');
   //res.send('Hello World!')
 })
 
