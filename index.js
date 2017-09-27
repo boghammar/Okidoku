4a515a04bb9f3e00eb8baa5f6ca76adc24a63a7b
Boghammar <S38333@corp1.ad1.seb.net>
2017-09-27 15:09:19 +0200

    unauth

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function checkAuth(req, res, next) {
     // don't serve /secure to those not logged in
     // you should add to this list, for each and every secure url
     if (req.url === '/norrtorp/adresser' && (!req.session || !req.session.authenticated)) {
-      res.render('unauthorised', { status: 403 });
+      res.render('/norrtorp/unauthorised.ejs', setRenderVars({ status: 403 }));
       return;
     }
   
