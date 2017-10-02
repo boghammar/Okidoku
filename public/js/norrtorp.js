d4d043ea962a54196463dc18df8b6226c6da3fe6
Anders Boghammar <anders@boghammar.com>
2017-10-02 17:54:23 +0200

    Filter on all

--- public/js/norrtorp.js
+++ public/js/norrtorp.js
@@ -3,6 +3,8 @@ window.addEventListener('load', function () {
         el: '#adresslista',
         data: {
             searchName: '',
+            searchMail: '',
+            searchProperty: '',
             adresser : [
                 {id: 1, name: 'Kalle', mail: 'a@b.com', fastighet: '3:2', validated: true},
                 {id: 2, name: 'Nisse', mail: 'a@b.se', fastighet: '3:12', validated: false},
@@ -11,7 +13,10 @@ window.addEventListener('load', function () {
         computed : {
             filteredItems() {
                 return this.adresser.filter(adress => {
-                    return adress.name.toLowerCase().indexOf(this.searchName.toLowerCase()) > -1;
+                    return adress.name.toLowerCase().indexOf(this.searchName.toLowerCase()) > -1
+                    && adress.mail.toLowerCase().indexOf(this.searchMail.toLowerCase()) > -1
+                    && adress.fastighet.toLowerCase().indexOf(this.searchProperty.toLowerCase()) > -1
+                    ;
                 })
             }
         },
@@ -19,7 +24,16 @@ window.addEventListener('load', function () {
             editAdress : function(id) {
                 console.log("Editing id="+id);
                 console.log("Name=" + this.$data.adresser[id].name);
+            },
+            removeAdress : function(id) {
+                console.log("Remove id="+id);
+                console.log("Name=" + this.$data.adresser[id].name);
+            },
+            sendValidation : function(id) {
+                console.log("Validate  id="+id);
+                console.log("Name=" + this.$data.adresser[id].name);
             }
+
         }
     });
 });
