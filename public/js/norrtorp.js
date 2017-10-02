876c9b5dda32fb537f0ef4b71b02d9d290dd6960
Boghammar <S38333@corp1.ad1.seb.net>
2017-10-02 15:36:48 +0200

    Filters

--- public/js/norrtorp.js
+++ public/js/norrtorp.js
@@ -2,10 +2,18 @@ window.addEventListener('load', function () {
     var app = new Vue({
         el: '#adresslista',
         data: {
+            searchName: '',
             adresser : [
                 {id: 1, name: 'Kalle', mail: 'a@b.com', fastighet: '3:2', validated: true},
                 {id: 2, name: 'Nisse', mail: 'a@b.se', fastighet: '3:12', validated: false},
             ]
+        },
+        computed : {
+            filteredItems() {
+                return this.adresser.filter(adress => {
+                    return adress.name.toLowerCase().indexOf(this.searchName.toLowerCase()) > -1;
+                })
+            }
         },
         methods : {
             editAdress : function(id) {
